2aabdaa73c678f3398217e788aed075b4fa3956a
James Yale <j.yale@livelinktechnology.net>
2016-11-10 15:47:23 +0000

    Create sysbench DB

--- mysql-config.sql
+++ mysql-config.sql
@@ -5,3 +5,5 @@ GRANT ALL ON *.* TO 'proxysqlmonitor'@'%';
 # SQL client user
 CREATE USER 'proxysqlclient'@'%' IDENTIFIED BY 'password';
 GRANT ALL ON *.* TO 'proxysqlclient'@'%';
+
+CREATE DATABASE 'sbtest'
